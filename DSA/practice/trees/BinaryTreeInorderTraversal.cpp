bf5761e66e7c175dad959a8e3d4832249863830c
hs <96301516+tmkayy@users.noreply.github.com>
2025-11-15 14:19:52 +0200

    loaf boi

---
+++ DSA/practice/trees/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+		vector<int> res;
+		traverse(root, res);
+		return res;
+	}
+
+	void traverse(TreeNode* root, vector<int>& res) {
+		if (!root)
+			return;
+		traverse(root->left, res);
+		res.push_back(root->val);
+		traverse(root->right, res);
+	}
+};

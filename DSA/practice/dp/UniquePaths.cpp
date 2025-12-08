74743a220c1d8d41fcb66fab1ffb1651815739ca
hs <96301516+tmkayy@users.noreply.github.com>
2025-12-08 14:18:41 +0200

    decades online

---
+++ DSA/practice/dp/UniquePaths.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int uniquePaths(int m, int n) {
+		vector<vector<int>> dp(m, vector<int>(n, 1));
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+			{
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+};

de02dfab706ea704807087df8452c20c7f757b98
haralampy slavkov <96301516+tmkayy@users.noreply.github.com>
2026-04-08 14:46:00 +0300

    fix

--- OS/shell-prep/2017-IN-03.sh
+++ OS/shell-prep/2017-IN-03.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-awk -F ':' '$5 ~ /[^ ]*а,,,,I$/ {print substr($1, 2, 2)}' /etc/passwd | sort | uniq -c | tail -n 1
+awk -F ':' '$5 ~ /[^ ]*а,,,,I$/ {print substr($1, 3, 2)}' /etc/passwd | sort | uniq -c | tail -n 1
+
+#no substr
+awk -F ':' '$5 ~ /[^ ]*а,,,,I$/ { match ($1, /^s[0-9]([0-9][0-9])/ , arr);print arr[1]}' /etc/passwd | sort | uniq -c | tail -n 1
